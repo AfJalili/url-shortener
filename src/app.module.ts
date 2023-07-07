715d21cd3fa5c2f1c1a5f75d69afa65aac978288
Afshin Jalili <a.jalili@kaman.ir>
2023-07-08 02:51:04 +0330

    feat(redis): implement cache functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UrlsModule } from './urls/urls.module';
-
+import { CacheModule } from '@nestjs/cache-manager';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const redisStore = require('cache-manager-redis-store').redisStore;
 @Module({
-  imports: [UrlsModule],
+  imports: [
+    UrlsModule,
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      // todo import configuration;
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/urls/urls.service.ts
+++ src/urls/urls.service.ts
@@ -1,18 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { nanoid } from 'nanoid';
-
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 @Injectable()
 export class UrlsService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
   async shortenUrl(url: string): Promise<string> {
-    const id = nanoid(8);
-    // todo add redis
-    // todo check if the url already exists
-    // await this.cache.set(id, url);
+    let id: string | undefined = await this.cacheManager.get(url);
+    if (!id) {
+      id = nanoid(8); //todo import size from config;
+      await Promise.all([
+        this.cacheManager.set(id, url, 0),
+        this.cacheManager.set(url, id, 0),
+      ]);
+    }
     return id;
   }
 
-  async findOne(id: string): Promise<string> {
-    // todo find url in redis
-    return 'url';
+  findOne(id: string): Promise<string | undefined> {
+    return this.cacheManager.get(id);
   }
 }
