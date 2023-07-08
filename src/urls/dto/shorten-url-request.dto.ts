c1724e2ae3601826b60fb99eb369bc97d3fa4c2b
Afshin Jalili <a.jalili@kaman.ir>
2023-07-08 15:09:42 +0330

    docs(): add swagger docs

--- src/urls/dto/shorten-url-request.dto.ts
+++ src/urls/dto/shorten-url-request.dto.ts
@@ -1,6 +1,8 @@
 import { IsNotEmpty, IsUrl } from 'class-validator';
+import { ApiProperty } from "@nestjs/swagger";
 
 export class ShortenUrlRequestDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsUrl()
   url: string;
